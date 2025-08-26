fd7231c6b322d19050faec5ded451dc7e84f8e5d
Santserrano <sant.serrano@icloud.com>
2025-08-26 18:02:44 -0300

    feat: form event

--- apps/web/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { SimplifiedAdminDashboard } from './simplified-admin-dashboard';
-export { SimpleDeploysList } from './simple-deploys-list';
-export { RealTimeLogs } from './real-time-logs';
