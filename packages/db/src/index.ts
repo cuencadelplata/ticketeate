a36ba72ce78f165e10428d691a100b9b2c45346a
Santserrano <sant.serrano@icloud.com>
2025-08-28 16:07:54 -0300

    prisma sync schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,6 +2,3 @@ import { PrismaClient } from '@prisma/client';
 
 // Export a singleton instance
 export const prisma = new PrismaClient();
-
-// Export types
-export type { User, Post } from '@prisma/client';
