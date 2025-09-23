528ee03d372726fd54d6d274270f09e13bf8cd2a
Santserrano <sant.serrano@icloud.com>
2025-09-23 16:55:09 -0300

    feat: design system components

---
+++ apps/next-frontend/app/diseno/componentes/page.tsx
@@ -0,0 +1,15 @@
+import { Sidebar } from "@/components/design-system/sidebar"
+import { ComponentsSection } from "@/components/design-system/sections/components"
+
+export default function ComponentsPage() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1 p-8 max-w-6xl">
+          <ComponentsSection />
+        </div>
+      </div>
+    </div>
+  )
+}
