36b6eb4f7caa260ba34b371b5c9c35b5552b4abd
Santserrano <sant.serrano@icloud.com>
2025-11-02 23:44:23 -0300

    cupones

--- scripts/test_enum.ts
+++
@@ -1,5 +0,0 @@
-import { change_type } from '@repo/db';
-
-console.log('change_type.UPDATE:', change_type.UPDATE);
-console.log('Type:', typeof change_type.UPDATE);
-console.log('All values:', Object.keys(change_type).map(k => `${k}: ${change_type[k as keyof typeof change_type]}`));
